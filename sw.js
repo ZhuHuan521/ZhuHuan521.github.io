bf94e8e4f868f5ae54ed27b9b06cd7d7e9cf58b2
ZhuHuan521 <zhuhuan250@outlook.com>
2025-10-28 00:11:40 +0800

    Site updated: 2025-10-28 00:11:40

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1761581180489";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1761581500059";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
